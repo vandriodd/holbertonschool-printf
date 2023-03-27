a1c2d3b8760fd311bf9b31ed997dc9381aa4b0ec
vandriodd <ludasaadi@gmail.com>
2023-03-27 08:03:49 -0700

    init repo

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Libraries */
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+/* Prototypes */
+int _printf(const char *format, ...); /* main func */
+
+#endif
