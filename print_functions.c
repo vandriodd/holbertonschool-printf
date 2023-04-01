880fba579470283f16238adabb2ab20d06820ba0
vandriodd <ludasaadi@gmail.com>
2023-04-01 12:01:10 -0700

    [Draft] print number func

--- print_functions.c
+++ print_functions.c
@@ -47,3 +47,49 @@ int print_percent(va_list args)
 	_write('%');
 	return (1);
 }
+
+/**
+ * print_number - prints a number
+ * @args: argument received
+ * Return: characters printed
+ */
+
+int print_number(va_list args)
+{
+	long int number = va_arg(args, int), n = number, value = 1;
+	int len = 0;
+	char i;
+
+	if (number < 0)
+	{
+		n *= -1;
+		number = -number;
+		_write('-');
+		len++;
+	}
+
+	if (number == 0)
+	{
+		_write('0');
+		len++;
+		return (len);
+	}
+
+	while (n > 0)
+	{
+		n /= 10;
+		value *= 10;
+	}
+
+	value /= 10; /* digit overshoots by one, fixes it */
+
+	while (value != 0)
+	{
+		i = number / value + '0'; /* obtains the 1st digit */
+		_write(i);
+		len++;
+		number %= value;
+		value /= 10; /* reduce digit */
+	}
+	return (len);
+}
